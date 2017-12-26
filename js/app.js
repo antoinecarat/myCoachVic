5a0bd2a233f15f2753b422b3880f04caf9cd0d05
MgxCherrueau <cherrueaumgx@gmail.com>
2017-12-26 14:06:04 +0100

    Add tabs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    currentTab:'home',
+    sports: [
+    	{name:'climbing'},
+    	{name:'bike'},
+    	{name:'rugby'},
+    	{name:'bouldering'}
+    ]
   }
 })
