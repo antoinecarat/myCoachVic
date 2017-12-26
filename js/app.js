985e2d1465d50e5cb566cc2dfb1f1340fde5d752
MgxCherrueau <cherrueaumgx@gmail.com>
2017-12-26 15:53:41 +0100

    Start creating overview

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ var app = new Vue({
   el: '#app',
   data: {
     currentTab:'home',
+    currentSport: 'all',
     sports: [
     	{name:'climbing'},
     	{name:'bike'},
     	{name:'rugby'},
     	{name:'bouldering'}
+    ],
+    chartKinds: [
+    	{name:'nbSession_total'},
+    	{name:'nbSession_monthAVG'},
+    	{name:'nbSession_weekAVG'},
+    	{name:'nbSession_totalAVG'}
     ]
   }
 })
