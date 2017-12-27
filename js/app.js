974dee8703f4668fcb0e2f8a49b52d7c4f8244f5
MgxCherrueau <cherrueaumgx@gmail.com>
2017-12-27 13:56:21 +0100

    Enhance newEntry + newSport

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ var app = new Vue({
     submitNewEntry: function () {
     	console.log(JSON.stringify(this.newEntry));
     	//TODO: Send newEntry to DB.
+    	this.clearNewEntry();
     },
     clearNewEntry: function () {
     	for (var p in this.newEntry){
@@ -62,23 +63,33 @@ var app = new Vue({
     	}
     },
     addMetric: function () {
-            this.newSport.metrics.push({
+        if (this.newSport.current.name != null && this.newSport.current.type != null && this.newSport.current.name != "" && this.newSport.current.type != ""){
+         	this.newSport.metrics.push({
             	name: this.newSport.current.name,
             	type: this.newSport.current.type
             });
+            this.clearMetric();
+        } else {
+        	window.alert("New metric's name AND type should be filled.")
+        }
     },
     clearMetric: function () {
     	this.newSport.current.name=null;
     	this.newSport.current.type=null;
     },
     submitNewSport: function () {
-    	console.log(JSON.stringify(this.newSport));
-    	//TODO: Send newSport to DB.
+		if (this.newSport.name != null && this.newSport.name != ""){
+	    	this.clearMetric();
+	    	console.log(JSON.stringify(this.newSport));
+	    	//TODO: Send newSport to DB without current.
+	    	this.clearNewSport();
+    	} else {
+        	window.alert("New sport should have a name.")
+        }
     },
     clearNewSport: function () {
     	this.newSport.name=null;
-    	this.newSport.current.name=null;
-    	this.newSport.current.type=null;
+    	this.clearMetric();
     	this.newSport.metrics=[];
     }
   }
