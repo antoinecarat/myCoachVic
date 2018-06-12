9291a6d2cc7d4c82efb387d815056e74ed901c9b
cara-puce <antoine.carat@gmail.com>
2018-06-13 00:51:47 +0200

    Start /newsport form with select from existing

--- vic-frontend/src/components/NewSport.vue
+++ vic-frontend/src/components/NewSport.vue
@@ -10,9 +10,35 @@
         </h2>
       </div>
     </div>
-    <div class="is-grouped">
-      <button class="button" @click="addSport()">Add</button>
-      <button class="button is-text" @click="$router.go(-1)">Cancel</button>
+    <div class="column">
+      <div class="field">
+        <label class="label">Pick up an existing sport</label>
+        <div class="control">
+          <div class="select">
+            <select v-model="selectedSport" @change="bindSport()">
+              <option>None</option>
+              <option v-for="sport in availableSports">{{sport.name}}</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="jdoe" :class="{'is-danger': sportAlreadyExists}" v-model="name" readonly v-if="existingSportSelected"/>
+          <input class="input" type="text" placeholder="jdoe" :class="{'is-danger': sportAlreadyExists}" v-model="name" v-else/>
+        </div>
+        <p class="help is-danger" v-if="sportAlreadyExists">This sport does exists: <a @click="">Add</a></p>
+      </div>
+      <div class="field">
+        <label class="label">Metrics</label>
+
+      </div>
+      <div class="is-grouped">
+        <button class="button" @click="addSport()">Add</button>
+        <button class="button is-text" @click="$router.go(-1)">Cancel</button>
+      </div>
     </div>
   </section>
 </template>
@@ -20,9 +46,26 @@
 <script>
 export default {
   name: 'NewSport',
+  data: function () {
+    return {
+      name: '',
+      selectedSport: 'None',
+      existingSportSelected: false,
+      wholeSports: []
+    }
+  },
   methods: {
+    bindSport: function () {
+      if (this.selectedSport !== 'None') {
+        this.existingSportSelected = true
+        this.name = this.selectedSport
+      } else {
+        this.existingSportSelected = false
+        this.name = ''
+      }
+    },
     addSport: function () {
-      // Add entry to db and store
+      // Add to store if already exists, add to db+store if not
       this.$router.push('/overview')
       this.$snackbar.open({
         message: 'New sport has been added',
@@ -34,6 +77,28 @@ export default {
         }
       })
     }
+  },
+  computed: {
+    sportAlreadyExists: function () {
+      return this.availableSports.filter(value => {
+        return value.name === this.name;
+      }, this).length > 0
+    },
+    availableSports: function() {
+      return this.wholeSports.filter(value => {
+        return !this.$store.state.sports.includes(value)
+      }, this)
+    }
+  },
+  mounted: function () {
+    /* axios.get('http://localhost:5000/listSports/')
+      .then(res => {
+        this.availableSports = res.data
+      })
+      .catch(err => {
+        console.log(err);
+      }) */
+    this.wholeSports = [{name: 'Rugby'}, {name: 'Cycling'}, {name: 'Bouldering'}, {name: 'Handball'}]
   }
 }
 </script>
